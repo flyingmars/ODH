d931c4c11cc5d56b73c586587d93bc303f94e9c9
Mars <flyingmars@gmail.com>
2021-09-08 16:19:26 +0800

    fix bug

--- src/dict/twtw_moedict.js
+++ src/dict/twtw_moedict.js
@@ -244,20 +244,13 @@ class twtw_moedict {
         let expression = word; //headword
         
         let reading = "" ;
-        if ( parsed["臺羅"] == word ) reading += "<span class='pos'>臺羅</span>";
-        if ( parsed["白話字"] == word ) reading += "<span class='pos'>白話字</span>";
-        let reading = `<br><span class='pos'>臺羅</span>${parsed["臺羅"]}<br><span class='pos'>白話字</span>${parsed["白話字"]}`;
+        if ( parsed["臺羅"] == word ) reading += `<span class='pos'>臺羅</span>`;
+        if ( parsed["白話字"] == word ) reading += `<span class='pos'>白話字</span>`;
+        reading += `<br><span class='pos'>臺羅</span>${parsed["臺羅"]}<br><span class='pos'>白話字</span>${parsed["白話字"]}`;
         
         let audios = [];
         
-        try {
-            // let soundReqUrl =  `https://hapsing.ithuan.tw/bangtsam?taibun=${encodeURIComponent(parsed["臺羅"])}`          
-          
-            // let soundData = await api.fetch(soundReqUrl);
-            audios[0] = `https://hapsing.ithuan.tw/bangtsam?taibun=${encodeURIComponent(parsed["臺羅"])}`;
-        } catch (err) {
-            audios = [];
-        }        
+        audios[0] = `https://hapsing.ithuan.tw/bangtsam?taibun=${encodeURIComponent(parsed["臺羅"])}`;
         
         
         //audios[1] = `http://dict.youdao.com/dictvoice?audio=${encodeURIComponent(expression)}&type=2`;
@@ -275,13 +268,13 @@ class twtw_moedict {
             // definition += `<li class="ec">${pos}<span class="ec_chn">${def}</span></li>`;
         // }
         // definition += '</ul>';
-        // let css = `
-            // <style>
-                // span.pos  {text-transform:lowercase; font-size:0.9em; margin-right:5px; padding:2px 4px; color:white; background-color:#0d47a1; border-radius:3px;}
-                // span.simple {background-color: #999!important}
-                // ul.ec, li.ec {margin:0; padding:0;}
-            // </style>`;
-        let css = this.renderCSS();
+        let css = `
+            <style>
+                span.pos  {text-transform:lowercase; font-size:0.9em; margin-right:5px; padding:2px 4px; color:white; background-color:#0d47a1; border-radius:3px;}
+                span.simple {background-color: #999!important}
+                ul.ec, li.ec {margin:0; padding:0;}
+            </style>`;
+        //let css = this.renderCSS();
         notes.push({
             css,
             expression,
